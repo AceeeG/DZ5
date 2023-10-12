dea41e67422190862c587f116ff27612cb1d9209
Гарифуллин Амир <amirgarifullin@gmail.com>
2023-10-12 11:03:04 +0300

    Доделал 2

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -33,10 +33,10 @@ namespace Exercises
     {
         static void ReadAllStudents(Dictionary<string, Student> students_dict)
         {
-            for (int i = 0; i < 10; i++) 
+            for (int i = 0; i < 10; i++)
             {
                 KeyValuePair<string, Student> student = students_dict.ElementAt(i);
-                Console.WriteLine($"{student.Value.surname} {student.Value.name} {student.Value.birth_year} {student.Value.exam} {student.Value.points}\n");
+                Console.WriteLine($"{student.Value.surname} {student.Value.name} {student.Value.birth_year} {student.Value.exam} {student.Value.points}");
             }
         }
 
@@ -46,11 +46,11 @@ namespace Exercises
 
             FileInfo student_file = new FileInfo(link[0]);
             Dictionary<string, Student> students_dict= new Dictionary<string, Student>();
+
+            string[] student_arr = File.ReadAllLines(link[0]);
             if (student_file.Exists)
             {
-                string[] student_arr = File.ReadAllLines(link[0]);
-
-                for(int i = 0; i < student_arr.Length; i++)
+                for (int i = 0; i < student_arr.Length; i++)
                 {
                     string[] student_data = student_arr[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
                     Student student = new Student();
@@ -61,6 +61,66 @@ namespace Exercises
                     "Введите <Удалить>, чтобы удалить студента\n" +
                     "Введите <Сортировать> чтобы отсортировать студентов по баллам\nВведите действие:\n");
                 ReadAllStudents(students_dict);
+
+                string choise = Console.ReadLine().ToLower();
+                switch (choise)
+                {
+                    case "новый студент":
+                        ReadAllStudents(students_dict);
+                        Student student1 = new Student();
+                        Console.WriteLine("Введите фамилию студента\n");
+                        student1.surname = Console.ReadLine();
+                        Console.WriteLine("Введите имя студента\n");
+                        student1.name = Console.ReadLine();
+                        Console.WriteLine("Введите год рождения студента\n");
+                        bool birth_year_flag = Int32.TryParse(Console.ReadLine(), out student1.birth_year);
+                        if (!birth_year_flag)
+                        {
+                            do
+                            {
+                                Console.WriteLine("Вы ввели не число, введите число\n");
+                                birth_year_flag = Int32.TryParse(Console.ReadLine(), out student1.birth_year);
+                            } while (birth_year_flag);
+                        }
+                        Console.WriteLine("Введите доп.экзамен(Информатика, Физика, Английский), который студент сдавал\n");
+                        student1.exam = Console.ReadLine();
+                        Console.WriteLine("Ввведите количество баллов по нему\n");
+                        bool points_flag = Int32.TryParse(Console.ReadLine(), out student1.points);
+                        if (!points_flag)
+                        {
+                            do
+                            {
+                                Console.WriteLine("Вы ввели не число, введите число\n");
+                                points_flag = Int32.TryParse(Console.ReadLine(), out student1.points);
+                            } while (points_flag);
+                        }
+                        students_dict.Add(student1.surname + student1.name, student1);
+                        ReadAllStudents(students_dict);
+                        break;
+                    case "удалить":
+                        Console.WriteLine("Введите фамилию и имя студента, которого нужно удалить\n");
+                        string key = Console.ReadLine();
+                        students_dict.Remove(key);
+                        if (students_dict.Remove(key))
+                        {
+                            Console.WriteLine($"Студент {key} был удалён\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Студент {key} не найден");
+                        }
+                        break;
+                    case "сортировать":
+                        students_dict = students_dict.OrderBy(exam => exam.Value.exam).ToDictionary(exam => exam.Key, student => student.Value);
+                        break;
+                    default:
+                        Console.WriteLine("Неверная команда");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Файл не найден");
             }
 
         }
